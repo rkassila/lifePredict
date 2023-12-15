49ad6882274171f58987d67e95d19b5389a745f5
Reda KASSI LAHLOU <rkassila@gmail.com>
2023-12-15 15:12:40 +0900

    Adding interface with streamlit

---
+++ predictApp/interface/main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from PIL import Image
+import requests
+import tempfile
+import io
+import time
+
+st.set_page_config(
+    page_title="Life Prediction",
+    layout='wide'
+)
+
+def app():
+    st.write("To be completed")
